6dbc3188332a9b68f7a02756cbd9e4aa6cfeaaf4
nonylene <cookie.speaker@gmail.com>
2015-11-04 00:10:20 +0900

    setup database script

---
+++ utils/setup_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+from os import path
+
+conn = sqlite3.connect(path.join(path.abspath(path.dirname(__file__)), ('../main.db')))
+
+sql = conn.cursor()
+sql.execute("drop table if exists mylist")
+sql.execute("""create table if not exists mylist (
+    id int primary key not null,
+    smid text unique,
+    category text,
+    date timestamp,
+    last_fetched_time timestamp,
+    json text
+    )""")
+sql.close()
+
+conn.commit()
+conn.close()
